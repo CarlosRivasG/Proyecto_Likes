d19054ae5b7936f8272608b2dfd3835be0bd55e5
CarlosR <cmrivas20@gmail.com>
2022-08-02 13:37:07 -0400

    proyecto de likes

---
+++ index.js
@@ -0,0 +1,29 @@
+var count= 0;
+const valor = document.querySelector("#count");
+
+function liker(){
+    count++ ;
+    valor.innerText= count + " like(s)";
+    console.log(count);
+
+}
+
+
+var counta = 0;
+const valora = document.querySelector("#counta");
+
+function like(){
+    counta++ ;
+    valora.innerText= counta + " like(s)";
+    console.log(count);
+
+}
+var contador = 0;
+const val = document.querySelector("#contador");
+
+function likers(){
+    contador++ ;
+    val.innerText= contador + " like(s)";
+    console.log(count);
+
+}
